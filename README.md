2e30e726288cacfc0661cfd40c1469132379e6fa
Miroslav Karpíšek <m.karpisek@icloud.com>
2021-05-09 13:39:46 +0200

    argparse

---
+++ README.md
@@ -0,0 +1,9 @@
+# Lottery ticket na MNISTnet
+spuštění:
+
+find_lt_mnist.py -d DATA -m MODEL [-p P_PERC] [-s STRATEGY]
+
+# Lottery ticket na tinyImageNet
+spuštění:
+
+find_lt_tinyimagenet.py -d DATA -m MODEL [-p P_PERC] [-s STRATEGY]
--- find_lt_mnist.py
+++ find_lt_mnist.py
@@ -1,31 +1,40 @@
 import os
-import  multiprocessing
+import argparse
+import multiprocessing
 from shrinkbench.experiment import LotteryTicketExperiment
 from shrinkbench.plot import plot_df, df_from_results
 
 multiprocessing.freeze_support()
 
+parser = argparse.ArgumentParser(description='Lottery ticket on MNIST Net')
+parser.add_argument('-d', '--data', type=str, help='path to training data folder', required=True)
+parser.add_argument('-m', '--model', help='path to stored weights', required=True)
+parser.add_argument('--epochs', help='number of epochs', default=5)
+parser.add_argument('--p_rate', help='prune percentage at each prune iteration', default=0.15)
+parser.add_argument('--p_perc', help='desired prune percentage', default=0.7)
+parser.add_argument('--strategy', help='pruning strategy [GlobalMagWeight|LayerMagWeight]', default='GlobalMagWeight') 
+
+args = parser.parse_args()
+
 # setup environment
-os.environ["DATAPATH"] = "../data"
-os.environ["WEIGHTSPATH"] = "../pretrained"
+os.environ["DATAPATH"] = args.data
+os.environ["WEIGHTSPATH"] = args.model
 
 # settings
-STRATEGY = "GlobalMagWeight"
-P_PERC = 0.7#0.02
-COMPRESSION = 1 / P_PERC
+COMPRESSION = 1 / args.p_perc
 
 if __name__ == "__main__":
     exp = LotteryTicketExperiment(
         dataset="MNIST",
         model="MnistNet",
-        strategy=STRATEGY,
+        strategy=args.strategy,
         compression=COMPRESSION,
-        train_kwargs={"epochs": 5,},
+        train_kwargs={"epochs": args.epochs,},
         dl_kwargs={"split_ratio": 0.8, "num_workers": 4},
         run_on_device=True,
         pretrained=False,
         logging=False,
-        pruning_rate=0.15
+        pruning_rate=args.p_rate
     )
 
     exp.run()
